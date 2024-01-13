e49d4f7af544d2d719cc9cc3787eb11fe9af4a1e
BrujoFurioso22 <diegobarbecho133@gmail.com>
2024-01-13 18:07:18 -0500

    ProyectoErasmus

--- erasmus/src/GENERAL/JUEGOS/Juego2Memoria/ContenedorDestino.js
+++ erasmus/src/GENERAL/JUEGOS/Juego2Memoria/ContenedorDestino.js
@@ -1,7 +1,37 @@
 import "./contImgs.css";
+import styled from "styled-components";
 
 // ContenedorDestino.js
 import React from "react";
+
+const ContenedorImagenesArrastradas = styled.div`
+  background-color: rgba(185, 185, 185, 0.159);
+  min-width: 200px;
+  width: fit-content;
+  height: 120px;
+  display: flex;
+  padding: 10px 20px;
+  border-radius: 20px;
+  border: 2px dashed rgba(152, 152, 152, 0.654);
+`;
+
+const DestinoContenedor = styled.div`
+  user-select: none;
+  width: fit-content;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  z-index: 50;
+  padding: 10px 25px;
+  filter: drop-shadow(5px 5px 6px #515151);
+
+  .destino-imagen {
+    object-fit: contain;
+  }
+  .destino-contenedor:hover {
+    cursor: not-allowed;
+  }
+`;
 
 const ContenedorDestino = ({ imagenesEnContenedor, dejar, removeImage }) => {
   const handleDrop = () => {
@@ -14,26 +44,30 @@ const ContenedorDestino = ({ imagenesEnContenedor, dejar, removeImage }) => {
 
   const handleImageClick = (id) => {
     // Eliminar la imagen del estado imagenesEnContenedor
-    const nuevasImagenes = imagenesEnContenedor.filter((imagen) => imagen.id !== id);
+    const nuevasImagenes = imagenesEnContenedor.filter(
+      (imagen) => imagen.id !== id
+    );
     removeImage(nuevasImagenes);
   };
 
   return (
-    <div
-      className="contenedor-imagenes-arrastradas"
+    <ContenedorImagenesArrastradas
       onDrop={handleDrop}
       onDragOver={handleDragOver}
     >
       {imagenesEnContenedor.map((imagen) => (
-        <div
+        <DestinoContenedor
           key={imagen.id}
-          className="destino-contenedor"
           onClick={() => handleImageClick(imagen.id)}
         >
-          <img className="destino-imagen" src={imagen.src} alt={imagen.nombre} />
-        </div>
+          <img
+            className="destino-imagen"
+            src={imagen.src}
+            alt={imagen.nombre}
+          />
+        </DestinoContenedor>
       ))}
-    </div>
+    </ContenedorImagenesArrastradas>
   );
 };
 
--- erasmus/src/GENERAL/JUEGOS/Juego2Memoria/Game2.js
+++ erasmus/src/GENERAL/JUEGOS/Juego2Memoria/Game2.js
@@ -1,9 +1,100 @@
 import React, { useEffect, useState } from "react";
 import jsonImages from "./images";
 import ContenedorDestino from "./ContenedorDestino";
-import "./Game2.css";
 import { ConsultaCartasJuego2 } from "CONFIG/BACKEND/Consultas/Juegos";
+import styled from "styled-components";
 
+const ContendorGlobal = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 30px 0;
+`;
+
+const ContendorContenido = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  gap: 10px;
+
+  .contendor-1 {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-column-gap: 2px;
+    grid-row-gap: 2px;
+  }
+  .contendor-2 {
+    min-width: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 20px;
+  }
+  .cuadro {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: blanchedalmond;
+    border: 4px outset rgba(0, 0, 0, 0.264);
+    border-radius: 10px;
+    padding: 10px;
+    transition: 0.5s ease all;
+    &:hover {
+      transform: scale(1.05);
+      cursor: pointer;
+      border: 4px dashed rgba(0, 0, 0, 0.264);
+    }
+  }
+  .cuadro-imagen {
+    width: 80px;
+    height: 80px;
+    object-fit: contain;
+  }
+`;
+
+const BotonJugar = styled.button`
+  padding: 10px 25px;
+  outline: none;
+  border: none;
+  border-radius: 7px;
+  transition: all 0.3s ease;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(137, 43, 226, 0.63);
+    color: aliceblue;
+    box-shadow: -2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const ContenedorCuadrosContenedores = styled.div`
+  width: min-content;
+  gap: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  & > * {
+    margin: 10px 0;
+  }
+  .correcto {
+    background-color: rgba(0, 128, 0, 0.438);
+  }
+  .erroneo {
+    background-color: rgba(128, 0, 0, 0.438);
+  }
+  .contenedor-imagenes-arrastrada{
+    width: 100px;
+  }
+`;
+const ContenedorCentrados = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export function Game2() {
   const [imagenesEnContenedor, setImagenesEnContenedor] = useState([]);
@@ -17,17 +108,17 @@ export function Game2() {
   const [mostrarJugar, setMostrarJugar] = useState(true);
   const [haJugado, setHaJugado] = useState(0);
 
-  const [numCartas,setNumCartas] = useState(3)
+  const [numCartas, setNumCartas] = useState(3);
 
-  const ConsultarNumeroCartas = async ()=>{
-    const res= await ConsultaCartasJuego2(localStorage.getItem("id"))
+  const ConsultarNumeroCartas = async () => {
+    const res = await ConsultaCartasJuego2(localStorage.getItem("id"));
     console.log(res);
-    if(res.length > 0){
-      setNumCartas(res[0].numCartas)
+    if (res.length > 0) {
+      setNumCartas(res[0].numCartas);
     }
-  }
+  };
   useEffect(() => {
-    ConsultarNumeroCartas()
+    ConsultarNumeroCartas();
   }, []);
 
   const handleDragStart = (id) => {
@@ -106,8 +197,8 @@ export function Game2() {
   };
 
   return (
-    <div className="conetendor-global">
-      <div className="contenedor-contenido">
+    <ContendorGlobal>
+      <ContendorContenido>
         <div className="contendor-1">
           {jsonImages.map((imagen, index) => (
             <div
@@ -127,21 +218,16 @@ export function Game2() {
           ))}
         </div>
         <div className="contendor-2">
-          <div>
-            <span>Numero de cartas: </span>
-            <label>{numCartas}</label>
-          </div>
-          <div>
-            <button
-              className="boton-jugar"
-              disabled={!mostrarJugar}
-              onClick={IniciarJuego}
-            >
+          <ContenedorCentrados>
+            <span>Numero de cartas: {numCartas}</span>
+          </ContenedorCentrados>
+          <ContenedorCentrados>
+            <BotonJugar disabled={!mostrarJugar} onClick={IniciarJuego}>
               {haJugado === 0 ? "Jugar" : "Jugar de nuevo"}
-            </button>
-          </div>
+            </BotonJugar>
+          </ContenedorCentrados>
           {iniciarJuego === 1 && (
-            <div className="contenedor-cuadros-contenedores">
+            <ContenedorCuadrosContenedores>
               <ContenedorDestino
                 imagenesEnContenedor={imagenesEnContenedor}
                 dejar={() => (editable ? handleDrop() : "")}
@@ -203,10 +289,10 @@ export function Game2() {
                   </span>
                 </div>
               )}
-            </div>
+            </ContenedorCuadrosContenedores>
           )}
         </div>
-      </div>
-    </div>
+      </ContendorContenido>
+    </ContendorGlobal>
   );
 }
